75549dd1a7bdbfe021d51902990f9bf13a9fe265
shreyans <30747740+shrey05@users.noreply.github.com>
2017-08-14 14:56:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ function createTemplate(data) {
     var date = data.date;
     var heading = data.heading;
     var content = data.content;
-var htmlTemplate = `
+    var htmlTemplate =  `
 <html>
 <head>
     <title>
