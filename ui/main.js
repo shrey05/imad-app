7e14c9036a11d743d1a801fab16d879e3bfdca87
shreyans <30747740+shrey05@users.noreply.github.com>
2017-08-20 15:08:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,5 +24,5 @@ button.onclick = function (){
   
   //  Make the request 
   request.open('GET', 'http://http://shreyanschordia11.imad.hasura-app.io/counter',true);
-  request.send(null);
+  request. send(null);
 };
